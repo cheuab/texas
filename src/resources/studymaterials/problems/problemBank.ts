828145e6d2242727ead26f2b4d0990ee97258685
Ty Loftin <tyloftin4@gmail.com>
2025-09-08 21:43:53 -0500

    added some more questions

--- src/resources/studymaterials/problems/problemBank.ts
+++ src/resources/studymaterials/problems/problemBank.ts
@@ -1,15 +1,32 @@
+import { title } from "process";
+
 export const problemSets = [
   {
     className: "ChE 210: Introduction to Computing",
     problems: [
       {
-        title: "Unit Conversion Practice",
-        description: "Convert between SI and Imperial units for common engineering quantities.",
+        title: "Units Practice",
+        description: "Convert between SI and Imperial units. Find unknown units.",
         link: "#"
       },
       {
-        title: "Dimensional Analysis Challenge",
-        description: "Check equations for dimensional consistency and solve for unknowns.",
+        title: "Graphing with Excel",
+        description: "Practice creating graphs and charts using Excel for data analysis.",
+        link: "#"
+      },
+      {
+        title: "Material Balances Practice",
+        description: "Solve problems involving material balances on various processes.",
+        link: "#"
+      },
+      {
+        title: "Atomic Balance Problems",
+        description: "Practice problems on atomic balances in chemical processes.",
+        link: "#"
+      },
+      {
+        title: "Solver Practice",
+        description: "Use Excel Solver to find process parameters.",
         link: "#"
       }
     ]
@@ -23,8 +40,38 @@ export const problemSets = [
         link: "#"
       },
       {
-        title: "Stoichiometry Practice",
-        description: "Solve problems involving chemical reactions and stoichiometry.",
+        title: "Transient Balance Problems",
+        description: "Solve problems involving transient balances in chemical processes.",
+        link: "#"
+      },
+      {
+        title: "Energy Balance Problem Set",
+        description: "Practice solving energy balances on various processes.",
+        link: "#"
+      },
+      {
+        title: "Reactive Material Balances",
+        description: "Problems on material balances involving chemical reactions.",
+        link: "#"
+      },
+      {
+        title: "Equilibirum Reactions Practice",
+        description: "Solve problems involving equilibrium reactions in chemical processes.",
+        link: "#"
+      },
+      {
+        title: "Solving different Reactors",
+        description: "Practice problems on solving different types of reactors.",
+        link: "#"
+      },
+      {
+        title: "PVT Problems",
+        description: "Practice problems on pressure-volume-temperature relationships and equations of state.",
+        link: "#"
+      },
+      {
+        title: "Combined Material and Energy Balances",
+        description: "Solve problems that involve both material and energy balances in chemical processes.",
         link: "#"
       }
     ]
@@ -38,10 +85,130 @@ export const problemSets = [
         link: "#"
       },
       {
-        title: "Mass Transfer Challenge",
+        title: "Mass Transfer Practice",
         description: "Practice problems on diffusion and mass transfer coefficients.",
+        link: "#"
+      },
+      {
+        title: "Macroscopic Fluid Mechanics Practice",
+        description: "Problems on fluid flow, pressure drop, and pipe flow.",
+        link: "#"
+      },
+      {
+        title: "Microscopic Fluid Mechanics Practice",
+        description: "Problems on laminar and turbulent flow, boundary layers, and velocity profiles.",
+        link: "#"
+      },
+      {
+        title: "Lumped Capacitance",
+        description: "Practice solving energy balances on various processes.",
+        link: "#"
+      }
+    ]
+  },
+  {
+    className: "ChE 322: Thermodynamics",
+    problems: [
+      {
+        title: "Open Systems Practice",
+        description: "Problems on open systems and control volumes.",
+        link: "#"
+      },
+      {
+        title: "Closed Systems Practice",
+        description: "Problems on closed systems and thermodynamic cycles.",
+        link: "#"
+      },
+      {
+        title: "Cycle Problems Practice",
+        description: "Practice problems on thermodynamic cycles and efficiency calculations.",
+        link: "#"
+      },
+      {
+        title: "Using equations of state for property estimation",
+        description: "Practice problems on using equations of state for estimating thermodynamic properties.",
+        link: "#"
+      }
+    ]
+  },
+  {
+    className: "ChE 348: Numerical Methods for Chemical Engineers",
+    problems: [
+      {
+        title: "Linear Programming Practice",
+        description: "Practice solving linear problems in chemical engineering.",
+        link: "#"
+      }
+    ]
+  },
+  {
+    className: "ChE 253K: Probability and Statistics",
+    problems: [
+      {
+        title: "Conditional Probability Problems",
+        description: "Practice problems on conditional probability.",
+        link: "#"
+      },
+      {
+        title: "Bayes Theorem Problems",
+        description: "Practice problems on Bayes' theorem and its applications.",
+        link: "#"
+      },
+      {
+        title: "Discrete Random Variables Practice",
+        description: "Practice problems on discrete random variables and their distributions.",
+        link: "#"
+      },
+      {
+        title: "Continuous Random Variables Practice",
+        description: "Practice problems on continuous random variables and their distributions.",
+        link: "#"
+      },
+      {
+        title: "Confidence Intervals Practice",
+        description: "Practice problems on confidence intervals and their interpretation.",
+        link: "#"
+      },
+      {
+        title: "Hypothesis Testing Practice",
+        description: "Practice problems on hypothesis testing and statistical significance.",
+        link: "#"
+      },
+      {
+        title: "Regression Analysis Practice",
+        description: "Practice problems on regression analysis and model fitting.",
+        link: "#"
+      },
+      {
+        title: "Design of Experiments Practice",
+        description: "Practice problems on designing experiments and analyzing results in JMP.",
+        link: "#"
+      }
+    ]
+  },
+  {
+    className: "ChE 354: Transport Processes",
+    problems: [
+      {
+        title: "Fluid Statics Problems",
+        description: "Practice problems on fluid statics and hydrostatics.",
+        link: "#"
+      },
+      {
+        title: "Momentum Balances",
+        description: "Practice problems on momentum balances in fluid systems.",
+        link: "#"
+      }
+    ]
+  },
+  {
+    className: "ChE 360: Process Control",
+    problems: [
+      {
+        title: "Simulink Practice",
+        description: "Practice problems on designing control systems using Simulink.",
         link: "#"
       }
     ]
   }
-];
+]
